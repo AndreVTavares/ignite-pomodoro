66ad8d598fe69c14f71d6cf26185ed5cd5995933
André Tavares <andretavares3@gmail.com>
2024-05-30 10:22:22 -0300

    feat:create the structure for History page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,15 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
